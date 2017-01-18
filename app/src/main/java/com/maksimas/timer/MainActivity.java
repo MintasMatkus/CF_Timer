8500401adaf552a417028dc422b28c50f66d0752
unknown <megas10000@yahoo.com>
2017-01-18 21:32:14 +0200

    Initial commit

---
+++ app/src/main/java/com/maksimas/timer/MainActivity.java
@@ -0,0 +1,116 @@
+package com.maksimas.timer;
+
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    SeekBar timerSeekBar;
+    TextView timerTextView;
+    Button controllerButton;
+    boolean counterIsActive = false;
+    CountDownTimer countDownTimer;
+
+    public void resetTimer() {
+
+        timerTextView.setText("0:00");
+        timerSeekBar.setProgress(0);
+        countDownTimer.cancel();
+        controllerButton.setText("Pradėti");
+        timerSeekBar.setEnabled(true);
+        counterIsActive = false;
+
+    }
+
+
+    public void updateTimer(int secondsLeft){
+
+        int minutes = secondsLeft / 60;
+        int seconds = secondsLeft - minutes * 60;
+
+        String secondString = Integer.toString(seconds);
+        if (seconds <= 9){
+
+            secondString = "0" + secondString;
+
+        }
+        timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+
+    }
+
+    public void controlTimer (View view){
+
+        if (counterIsActive == false){
+
+
+            counterIsActive = true;
+            timerSeekBar.setEnabled(false);
+            controllerButton.setText("Baigti");
+
+            countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {
+
+                @Override
+                public void onTick(long millisUntilFinished) {
+
+                    updateTimer((int) millisUntilFinished / 1000);
+
+                }
+
+                @Override
+                public void onFinish() {
+
+                    resetTimer();
+                    Toast.makeText(MainActivity.this, "Laikas suėjo", Toast.LENGTH_LONG).show();
+
+                }
+            }.start();
+        } else {
+
+            resetTimer();
+
+        }
+    }
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    timerSeekBar = (SeekBar) findViewById(R.id.timerSeekBar);
+    timerTextView = (TextView) findViewById(R.id.timerTextView);
+    controllerButton = (Button)findViewById(R.id.controllerButton);
+
+    timerSeekBar.setMax(600);
+    timerSeekBar.setProgress(0);
+    timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+
+        @Override
+        public void onProgressChanged (SeekBar seekBar,int progress, boolean fromUser){
+
+        updateTimer(progress);
+
+    }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar){
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar){
+
+        }
+    });
+
+
+}}
